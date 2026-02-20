cea0630928a7383fb295bace2a7fc96e3515938f
YashP_727 <48578747+yash-pant@users.noreply.github.com>
2026-02-19 19:35:41 -0800

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,13 @@
+RANDOM_SEED = 42
+MODEL_PATH = "models/champion_model.joblib"
+
+FEATURES = [
+    "squad_value_m",
+    "avg_age",
+    "wage_index",
+    "squad_depth",
+    "injury_risk",
+    "manager_stability",
+    "attack_index",
+    "defense_index",
+]
